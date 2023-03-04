863c2dc695cec62ed274656f338933b13cf5af17
Sahra Di Bernardi <sah.dibernardi@gmail.com>
2023-03-04 01:50:12 -0300

    add: requirement 2

--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -5,11 +5,24 @@ const router = express.Router();
 
 router.get('/talker', async (req, res) => {
     const talker = await getTalkers.getAllTalkers();
-    if (talker.lengh !== 0) {
+    if (talker.lenght !== 0) {
         res.status(200).json({ talker }.talker);
     } else {
         res.status(200).json([]);
     }
   });
+
+router.get('/talker/:id', async (req, res) => {
+const talker = await getTalkers.getAllTalkers();
+const { id } = req.params;
+const filtered = talker.find((t) => t.id === Number(id));
+if (filtered) {
+    res.status(200).json({ filtered }.filtered);
+} else {
+    res.status(404).json({
+        message: 'Pessoa palestrante não encontrada',
+      });
+}
+});
   
 module.exports = router;
